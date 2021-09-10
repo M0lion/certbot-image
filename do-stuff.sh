5c482ad9d98634479fe2f9677141d2146cfb8736
M0lion <bjorn-moholt@hotmail.com>
2021-09-10 16:38:08 +0200

    Update do-stuff.sh

--- do-stuff.sh
+++ do-stuff.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 echo foo
 /root/.acme.sh/acme.sh --issue --standalone -d molion.dev
-cat /root/.acme.sh/molion.dev/molion.dev.cert | echo
-cat /root/.acme.sh/molion.dev/molion.dev.key | echo
 
-CERT=cat /root/.acme.sh/molion.dev/molion.dev.cer | base64
-KEY=cat /root/.acme.sh/molion.dev/molion.dev.key | base64
-
-echo $CERT
-echo $KEY
-
-sed -i 's/#cert/$CERT/g' secret.yaml
-sed -i 's/#key/$KEY/g' secret.yaml
-cat secret.yaml | echo
-kubectl apply -f secret.yaml
-sleep 9999
+mail -s "cert" bjorn-moholt@hotmail.com < /root/.acme.sh/molion.dev/molion.dev.cer
+mail -s "key" bjorn-moholt@hotmail.com < /root/.acme.sh/molion.dev/molion.dev.key
