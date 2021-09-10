946de397ead261208423d6c287c3aa13ddb779e6
M0lion <bjorn-moholt@hotmail.com>
2021-09-10 15:40:34 +0200

    Update do-stuff.sh

--- do-stuff.sh
+++ do-stuff.sh
@@ -3,7 +3,7 @@ echo foo
 /root/.acme.sh/acme.sh --issue --standalone -d molion.dev
 cat /root/.acme.sh/molion.dev/molion.dev.cert | echo
 cat /root/.acme.sh/molion.dev/molion.dev.key | echo
-sed -i 's/#cert/$(cat /root/.acme.sh/molion.dev/molion.dev.cert)/g' secret.yaml
-sed -i 's/#key/$(cat /root/.acme.sh/molion.dev/molion.dev.key)/g' secret.yaml
+sed -i 's/#cert/$(cat /root/.acme.sh/molion.dev/molion.dev.cert | base64)/g' secret.yaml
+sed -i 's/#key/$(cat /root/.acme.sh/molion.dev/molion.dev.key | base64)/g' secret.yaml
 echo secret.yaml
 kubectl apply -f secret.yaml
