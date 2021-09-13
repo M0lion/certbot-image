8d7b56350112c382c9359036461ea532709b26ae
Bjørn Moholt <bjorn.moholt@consult.nordea.com>
2021-09-13 20:55:12 +0200

    Generalization

--- do-stuff.sh
+++ do-stuff.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 echo foo
-/root/.acme.sh/acme.sh --issue --standalone -d molion.dev
+/root/.acme.sh/acme.sh --issue --standalone -d $DOMAIN
 
-mail -s "cert" bjorn-moholt@hotmail.com < /root/.acme.sh/molion.dev/molion.dev.cer
-mail -s "key" bjorn-moholt@hotmail.com < /root/.acme.sh/molion.dev/molion.dev.key
+mail -s "cert" bjorn-moholt@hotmail.com < /root/.acme.sh/$DOMAIN/$DOMAIN.cer
+mail -s "key" bjorn-moholt@hotmail.com < /root/.acme.sh/$DOMAIN/$DOMAIN.key
 
-sed "s/#cert/$(base64 --wrap=0 /root/.acme.sh/molion.dev/molion.dev.cer)/g" secret.yaml -i
-sed "s/#key/$(base64 --wrap=0 /root/.acme.sh/molion.dev/molion.dev.key)/g" secret.yaml -i
+sed "s/#cert/$(base64 --wrap=0 /root/.acme.sh/$DOMAIN/$DOMAIN.cer)/g" secret.yaml -i
+sed "s/#key/$(base64 --wrap=0 /root/.acme.sh/$DOMAIN/$DOMAIN.key)/g" secret.yaml -i
+sed "s/#namespace/$NAMESPACE/g" secret.yaml -i
 
 cat secret.yaml
 
